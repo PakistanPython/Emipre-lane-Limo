88df3702cd445657da34c7af29c1a4c34f4451e7
PakistanPython <pakistanpython@gmail.com>
2025-08-04 17:50:01 +0500

    google login add

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import App from "./App";
 import "./styles/tailwind.css";
 import "./styles/index.css";
@@ -7,4 +8,8 @@ import "./styles/index.css";
 const container = document.getElementById("root");
 const root = createRoot(container);
 
-root.render(<App />);
+root.render(
+  <GoogleOAuthProvider clientId="190978031330-5r8ilt7gd9pde0gi6t0sk9d6184b5ofp.apps.googleusercontent.com">
+    <App />
+  </GoogleOAuthProvider>
+);
